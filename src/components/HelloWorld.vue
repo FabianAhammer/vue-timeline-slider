be21867adaaf1f22cc5b555e47ea8b8292ef2b72
Fabian Ahammer <fabian.ahammer@skylinx.aero>
2021-08-26 21:19:26 +0200

    content

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,122 +1,261 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br />
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener"
-        >vue-cli documentation</a
-      >.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel"
-          target="_blank"
-          rel="noopener"
-          >babel</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-pwa"
-          target="_blank"
-          rel="noopener"
-          >pwa</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint"
-          target="_blank"
-          rel="noopener"
-          >eslint</a
-        >
-      </li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li>
-        <a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a>
-      </li>
-      <li>
-        <a href="https://forum.vuejs.org" target="_blank" rel="noopener"
-          >Forum</a
-        >
-      </li>
-      <li>
-        <a href="https://chat.vuejs.org" target="_blank" rel="noopener"
-          >Community Chat</a
-        >
-      </li>
-      <li>
-        <a href="https://twitter.com/vuejs" target="_blank" rel="noopener"
-          >Twitter</a
-        >
-      </li>
-      <li>
-        <a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a>
-      </li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li>
-        <a href="https://router.vuejs.org" target="_blank" rel="noopener"
-          >vue-router</a
-        >
-      </li>
-      <li>
-        <a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-devtools#vue-devtools"
-          target="_blank"
-          rel="noopener"
-          >vue-devtools</a
-        >
-      </li>
-      <li>
-        <a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener"
-          >vue-loader</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-          rel="noopener"
-          >awesome-vue</a
-        >
-      </li>
-    </ul>
+  <div class="page-pwa">
+    <div class="header-pwa">Ordinary Header</div>
+    <div class="timeline-container">
+      <div class="timeline-header">My Timeline</div>
+      <div class="timeline-container-inner">
+        <div class="timeline-container-inner-bg">
+          <div class="timeline-head-container">
+            <div class="bwd-ico">
+              <button v-on:click="moveslider(true)" class="ico-btn">
+                &lt;
+              </button>
+            </div>
+            <div class="fwd-ico">
+              <button v-on:click="moveslider(false)" class="ico-btn">></button>
+            </div>
+          </div>
+          <div
+            ref="timeline"
+            v-on:mousedown="mousedown"
+            v-on:mousemove="mousemove"
+            v-on:mouseleave="isDown = false"
+            v-on:mouseup="
+              isDown = false;
+              beginMomentumTracking;
+            "
+            class="timeline-content"
+          >
+            <div
+              class="timeline-item-container"
+              v-for="(number, index) in Array(50)"
+              :key="index"
+            >
+              <Waypoint v-bind:input="index"></Waypoint>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
-<script>
+<script lang="js">
+import Waypoint from "./Waypoint.vue";
 export default {
-  name: "HelloWorld",
-  props: {
-    msg: String,
+  name: "SS",
+  components: {
+    Waypoint,
+  },
+  data() {
+    return {
+      isDown: false,
+      slider: null,
+      velX: 0,
+      scrollLeft: 0,
+      startX: 0,
+      momentumID: null,
+    };
+  },
+  methods: {
+    moveslider(positiveBool){
+      if(positiveBool === true){
+
+        this.$refs.timeline.scrollLeft -= 260;
+      }
+      else if(positiveBool === false){
+        this.$refs.timeline.scrollLeft += 260;
+      }
+    },
+    mousedown(e) {
+      this.isDown = true;
+      this.startX = e.pageX - this.$refs.timeline.offsetLeft;
+      this.scrollLeft = this.$refs.timeline.scrollLeft;
+      this.cancelMomentumTracking();
+    },
+    mousemove(e) {
+      if (!this.isDown) return;
+      const slider = this.$refs.timeline;
+      e.preventDefault();
+      const x = e.pageX - slider.offsetLeft;
+      const walk = (x - this.startX) * 3;
+      // Store the previous scroll position
+      var prevScrollLeft = slider.scrollLeft;
+      slider.scrollLeft = this.scrollLeft - walk;
+      // Compare change in position to work out drag speed
+      this.velX = slider.scrollLeft - prevScrollLeft;
+    },
+    beginMomentumTracking() {
+      this.momentumID = requestAnimationFrame(this.momentumLoop);
+    },
+    cancelMomentumTracking() {
+      cancelAnimationFrame(this.momentumID);
+    },
+    momentumLoop() {
+      this.$refs.timeline.scrollLeft += this.velX;
+      this.velX *= 0.8;
+      if (Math.abs(this.velX) > 0.8) {
+        this.momentumID = requestAnimationFrame(this.momentumLoop);
+      }
+    },
   },
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
+<style>
+.timeline-item-container {
+  display: flex;
+  padding: 0px 80px;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+.timeline-items {
+  width: 14px;
+  height: 70%;
+  margin-top: 50px;
+  background-color: red;
+  padding: 0px 10px 0px 10px;
+  margin: 0px 80px 0px 80px;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+.timeline-items:hover {
+  animation: larger-bg-x3 1s linear both;
 }
-a {
-  color: #42b983;
+@keyframes larger-bg-x3 {
+  0% {
+    transform: scale(1);
+  }
+
+  100% {
+    transform: scale(1.3);
+  }
+}
+
+.timeline-item-container:hover > .message-on-info {
+  animation: fade-in 1s linear both;
+}
+@keyframes larger-bg-x3 {
+  0% {
+    opacity: 0;
+  }
+
+  100% {
+    opacity: 1;
+  }
+}
+
+.timeline-container-inner-bg {
+  height: 100%;
+  width: 100%;
+  background: rgba(146, 146, 146, 0.219);
+  padding: 11px;
+  border: rgba(85, 85, 85, 0.356) solid 1px;
+  border-radius: 15px;
+}
+
+.timeline-content {
+  padding-top: 26px;
+  height: 90%;
+  overflow-x: scroll;
+  width: 68vw;
+  display: flex;
+  flex: none;
+  flex-direction: hidden;
+  overflow-x: hidden;
+  overflow-y: hidden;
+}
+.ico-btn {
+  font-weight: bold;
+  line-height: 1;
+  color: white;
+  font-size: 1.4em;
+  border-radius: 1em;
+  border: 0.1px solid white;
+  width: 28px;
+  height: 28px;
+}
+.timeline-header {
+  margin-bottom: 25px;
+  color: white;
+  width: 100%;
+  text-align: center;
+  font-size: 1.2em;
+}
+.fwd-ico {
+  margin-right: 10px;
+}
+.bwd-ico {
+  margin-left: 10px;
+}
+
+.timeline-head-container {
+  display: flex;
+  justify-content: space-between;
+  height: 10%;
+  width: 100%;
+  align-items: center;
+}
+.timeline-container-inner {
+  margin-left: 15%;
+  margin-right: 15%;
+  background: linear-gradient(
+    to bottom,
+    transparent 56%,
+    white 56%,
+    white 58%,
+    transparent 58%
+  );
+  width: 70%;
+  height: 100%;
+}
+
+.timeline-container {
+  margin-top: 10%;
+  width: 100%;
+  height: 30vh;
+}
+.page-pwa {
+  height: 100vh;
+  width: 100vw;
+  background-color: rgb(43, 41, 41);
+}
+
+.header-pwa {
+  height: 10vh;
+  width: 100%;
+  display: flex;
+  color: white;
+  font-size: 5em;
+  text-align: center;
+  justify-content: center;
+  font-family: "Arial";
+  -webkit-animation: focus-in-expand 0.8s cubic-bezier(0.25, 0.46, 0.45, 0.94)
+    both;
+  animation: focus-in-expand 0.8s cubic-bezier(0.25, 0.46, 0.45, 0.94) both;
+}
+
+@-webkit-keyframes focus-in-expand {
+  0% {
+    letter-spacing: -0.5em;
+    -webkit-filter: blur(12px);
+    filter: blur(12px);
+    opacity: 0;
+  }
+  100% {
+    -webkit-filter: blur(0);
+    filter: blur(0);
+    opacity: 1;
+  }
+}
+@keyframes focus-in-expand {
+  0% {
+    letter-spacing: -0.5em;
+    -webkit-filter: blur(12px);
+    filter: blur(12px);
+    opacity: 0;
+  }
+  100% {
+    -webkit-filter: blur(0);
+    filter: blur(0);
+    opacity: 1;
+  }
 }
 </style>
---
+++ src/components/Waypoint.vue
@@ -0,0 +1,108 @@
+<template>
+  <div class="waypoint-dom">
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      xmlns:bx="https://boxy-svg.com"
+      viewBox="0 0 40 60"
+    >
+      <defs>
+        <linearGradient
+          gradientUnits="userSpaceOnUse"
+          x1="60"
+          y1="60"
+          x2="110"
+          y2="110"
+          id="gradient-0"
+        >
+          <stop offset="0" class="start-color" />
+          <stop offset="100%" class="end-color" />
+        </linearGradient>
+        <linearGradient
+          gradientUnits="userSpaceOnUse"
+          x1="12"
+          y1="12"
+          x2="27"
+          y2="27"
+          id="gradient-1"
+        >
+          <stop offset="0" class="start-color-2" />
+          <stop offset="100%" class="end-color-2" />
+        </linearGradient>
+      </defs>
+      <g
+        transform="matrix(0.97665, 0, 0, 1, -77.199661, -61.783298)"
+        class="marker-base"
+        style="fill: url('#gradient-0')"
+      >
+        <path
+          d="M 89.449 64.616 C 96.375 60.633 104.198 61.236 110.345 64.543 C 113.133 66.043 118.64 72.17 118.782 81.192 C 118.926 90.353 112.448 97.404 109.161 102.485 C 106.318 106.88 101.916 118.128 100 118.095 C 98.391 118.067 93.124 106.064 90.455 102.339 C 88.068 99.008 81.227 91.323 80.804 82.391 C 80.474 75.438 83.064 68.369 89.449 64.616 Z"
+          style="fill: url('#gradient-0')"
+        />
+      </g>
+      <circle
+        style="fill: url('#gradient-1')"
+        cx="20.91"
+        cy="19.774"
+        r="11.664"
+        transform="matrix(0.97665, 0, 0, 1, -0.000005, -0.158351)"
+        class="marker-circle"
+      />
+    </svg>
+    <div class="waypoint-str-container">
+      <div class="waypoint-str">{{ input }}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "WaypointSVG",
+  props: {
+    input: Number,
+  },
+};
+</script>
+
+<style scoped>
+.waypoint-str-container {
+  width: 100%;
+  text-align: center;
+}
+
+.waypoint-str {
+  color: white;
+}
+
+.waypoint-dom > svg {
+  display: flex;
+  width: 100px;
+  justify-content: center;
+  align-items: center;
+}
+
+.waypoint-dom:hover {
+  animation: get-big 0.5s linear both;
+}
+
+@keyframes get-big {
+  0% {
+    transform: scale(1);
+  }
+  100% {
+    transform: scale(1.1);
+  }
+}
+
+.start-color {
+  stop-color: red;
+}
+.end-color {
+  stop-color: blue;
+}
+.start-color-2 {
+  stop-color: rgba(255, 255, 255, 0.856);
+}
+.end-color-2 {
+  stop-color: rgb(0, 0, 0);
+}
+</style>
